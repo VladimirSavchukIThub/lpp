4489f360e855f7e69a47d325e539050eb39b07c6
VladimirSavchukIThub <113594484+VladimirSavchukIThub@users.noreply.github.com>
2025-03-01 14:49:37 +0300

    db connect, docker tests, category, pytests

---
+++ app/routes/categories.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from unicodedata import category
+
+from app.database import SessionLocal
+from app.crud import create_user, create_category
+from app.schemas import UserCreate, CategoryCreate
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/categories/")
+def register_category(category: CategoryCreate, db: Session = Depends(get_db)):
+    return create_category(db, category)
